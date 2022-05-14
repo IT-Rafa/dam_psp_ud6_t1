bcb3c1c37bcef45c530a9dea6458746faf1d47b0
it-rafa <it-rafamartinez@outlook.es>
2022-05-14 15:08:50 +0200

    init maven project

---
+++ src/main/java/es/itrafa/dam_psp_ud6_t1_c/Init.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package es.itrafa.dam_psp_ud6_t1_c;
+
+/**
+ *
+ * @author it-ra
+ */
+public class Init {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
